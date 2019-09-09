c746c924e74b7880e39aed185bef2724fb34909c
happycooldude <pieterhuisman2000@gmail.com>
2019-09-09 13:55:20 +0200

    test met meerdere button prompts

--- b1w2l1/b1w2l1.js
+++ b1w2l1/b1w2l1.js
@@ -1,1 +1,25 @@
-// begin hier je JavaScript commandos
+alert("We hebben een aantal gegevens van u nodig");
+
+function mijnFunctie1(){
+  var person = prompt("Vul hier uw voornaam in", "Jan ");
+  if (person != null){
+    document.getElementById("demo").innerHTML =
+    "Hallo " + person + "! Hoe gaat het met u?"
+  }
+}
+
+function mijnFunctie2(){
+  var person = prompt("Vul hier uw achternaam in", "Alleman");
+  if (person != null){
+    document.getElementById("demo").innerHTML =
+    "Hallo " + person + "! Hoe gaat het met u?"
+  }
+}
+
+function mijnFunctie3(){
+  var person = prompt("Vul hier uw leeftijd in", "24");
+  if (person != null){
+    document.getElementById("demo").innerHTML =
+    "Hallo " + person + "! Hoe gaat het met u?"
+  }
+}

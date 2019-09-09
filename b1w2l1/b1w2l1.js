aa8607c1d2dde463c6277536283bd82f3e2905c5
happycooldude <pieterhuisman2000@gmail.com>
2019-09-09 14:27:16 +0200

    Programma werkt bijna goed alleen nog witregels laten toevoegen

--- b1w2l1/b1w2l1.js
+++ b1w2l1/b1w2l1.js
@@ -1,6 +1,9 @@
+//© Pieter Huisman 99047256
+//Applicatie en media ontwikkelaar
+
 alert("We hebben een aantal gegevens van u nodig");
 
-function mijnFunctie1(){
+function mijnFunctie1(){ // deze functie zorgt ervoor dat je een popupvenster krijgt waar je je voornaam kunt invullen
   var voornaam = prompt("Vul hier uw voornaam in", "Jan ");
   if (voornaam != null){
     document.getElementById("demo").innerHTML =
@@ -8,7 +11,7 @@ function mijnFunctie1(){
   }
 }
 
-function mijnFunctie2(){
+function mijnFunctie2(){ // deze functie zorgt ervoor dat je een popupvenster krijgt waar je je achternaam kunt invullen
   var achternaam = prompt("Vul hier uw achternaam in", "Alleman");
   if (achternaam != null){
     document.getElementById("demo").innerHTML =
@@ -16,15 +19,15 @@ function mijnFunctie2(){
   }
 }
 
-function mijnFunctie3(){
+function mijnFunctie3(){ // deze functie zorgt ervoor dat je een popupvenster krijgt waar je je leeftijd kunt invullen
   var leeftijd = prompt("Vul hier uw leeftijd in", "24");
   if (leeftijd != null){
     document.getElementById("demo").innerHTML =
-    leeftijd + " wat een mooie leeftijd!"
+    leeftijd + " jaar? wat een mooie leeftijd!"
   }
 }
 
-function test(){
+function test(){ // dit is een test functie waar je in 1 keer je voor en achternaam en leeftijd in kunt vullen
   var voornaam = prompt("Vul hier uw voornaam in");
   var achternaam = prompt("vul hier uw achternaam in");
   var leeftijd = prompt("vul hier uw leeftijd in");

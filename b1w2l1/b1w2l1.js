af5f74825e80c2636883cc01bea3b088c424e5e4
happycooldude <pieterhuisman2000@gmail.com>
2019-09-09 14:14:59 +0200

    test om meerdere gegevens in te vullen

--- b1w2l1/b1w2l1.js
+++ b1w2l1/b1w2l1.js
@@ -1,25 +1,35 @@
 alert("We hebben een aantal gegevens van u nodig");
 
 function mijnFunctie1(){
-  var person = prompt("Vul hier uw voornaam in", "Jan ");
-  if (person != null){
+  var voornaam = prompt("Vul hier uw voornaam in", "Jan ");
+  if (voornaam != null){
     document.getElementById("demo").innerHTML =
-    "Hallo " + person + "! Hoe gaat het met u?"
+    "Hallo " + voornaam + "! Hoe gaat het met u?"
   }
 }
 
 function mijnFunctie2(){
-  var person = prompt("Vul hier uw achternaam in", "Alleman");
-  if (person != null){
+  var achternaam = prompt("Vul hier uw achternaam in", "Alleman");
+  if (achternaam != null){
     document.getElementById("demo").innerHTML =
-    "Hallo " + person + "! Hoe gaat het met u?"
+    achternaam + " wat een leuke naam!"
   }
 }
 
 function mijnFunctie3(){
-  var person = prompt("Vul hier uw leeftijd in", "24");
-  if (person != null){
+  var leeftijd = prompt("Vul hier uw leeftijd in", "24");
+  if (leeftijd != null){
     document.getElementById("demo").innerHTML =
-    "Hallo " + person + "! Hoe gaat het met u?"
+    leeftijd + " wat een mooie leeftijd!"
+  }
+}
+
+function test(){
+  var voornaam = prompt("Vul hier uw voornaam in");
+  var achternaam = prompt("vul hier uw achternaam in");
+  var leeftijd = prompt("vul hier uw leeftijd in");
+  if (leeftijd != null){
+    document.getElementById("demo").innerHTML =
+      "Hallo" + " " + voornaam + " " + achternaam + " " + leeftijd + " " + "welkom op deze site"
   }
 }
